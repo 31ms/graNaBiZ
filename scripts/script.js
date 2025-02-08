d6864440799e5080411a5cd7e0802f600c4e4985
Kenaj08 <jan.borys.08@interia.pl>
2025-02-08 19:12:10 +0100

    Added: fps counter and simple performance test for collision detection

--- scripts/script.js
+++ scripts/script.js
@@ -13,33 +13,45 @@ class Player extends Object2D{
   }
 }
 let objects = []
-for(let i = 0; i<10; i++){
+for(let i = 0; i<30; i++){
   let j = new PhysicsObject2D(Geometry.rect,0)
   j.size = new Vector2(100,100)
-  j.position = new Vector2(250*Math.random() - 125,250*Math.random() - 125)
-  objects.push()
+  j.position = new Vector2(0,0)
+  objects.push(j)
 }
 let a = new PhysicsObject2D(Geometry.rect,0)
-a.size = new Vector2(100,50)
-a.zindex = -1
-ImageLoader.loadImage("../testbackground.png").then((img) => {
-  a.texture = new Texture().uploadImage(img)
-})
+// a.size = new Vector2(100,50)
+// a.zindex = -1
+// ImageLoader.loadImage("../testbackground.png").then((img) => {
+//   a.texture = new Texture().uploadImage(img)
+// })
 let dt = 0
 let c = 0
 var background = new Background()
+const times = []
 function frame(){
-  dt = new Date().getMilliseconds()
   CanvasScreen.fillWindow()
-  console.log(CollisionChecker.checkCollisions(0,a))
+  countFps()
   c+=0.02
-  a.position.y = 250*Math.sin(c)
+  for(let i = 0; i<objects.length; i++){
+    console.log(    CollisionChecker.checkCollisions(0,objects[i]).length)
+  }
+  // a.position.y = 250*Math.sin(c)
+  // console.log(CollisionChecker.checkCollisions(0,a).length)
   background.align()
   Renderer.clear()
   Renderer.render(Object2D.scene)
-  dt = Math.abs(dt - new Date().getMilliseconds())
 }
-setInterval(frame,16)
+let fpsCounter = document.getElementById("fpsCounter")
+function countFps(){
+  const now = performance.now()
+  while (times.length > 0 && times[0] <= now - 1000) {
+    times.shift()
+  }
+  times.push(now)
+  fpsCounter.innerText = `Fps: ${times.length}`
+}
+setInterval(frame,1)
 ImageLoader.loadImage("../testbackground.png").then((img) => {
   background.setImage(img)
 })
