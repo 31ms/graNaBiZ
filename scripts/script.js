700ce00c31a462badc3d9b4baf28348ce45fabd2
Kenaj08 <jan.borys.08@interia.pl>
2025-02-08 17:52:07 +0100

    deleted some comments from script.js

--- scripts/script.js
+++ scripts/script.js
@@ -12,17 +12,10 @@ class Player extends Object2D{
     this.velocity.y = 20
   }
 }
-
-
-// var player = new Player(Geometry.rect)
-// player.size = new Vector2(30,60)
-// player.render()
 let a = new PhysicsObject2D(Geometry.rect)
 let b = new PhysicsObject2D(Geometry.rect)
 a.size = new Vector2(100,50)
 b.size = new Vector2(50,100)
-// a.render()
-// b.render()
 let dt = 0
 let c = 0
 function frame(){
@@ -35,8 +28,6 @@ function frame(){
   c+=0.04
   a.position.y = 200*Math.sin(c)
   b.position.y = 200*Math.sin(c/2)
-  // player.velocity.addTo(player.acceleration.divide(16))
-  // player.position.addTo(player.velocity)
   dt = Math.abs(dt - new Date().getMilliseconds())
   console.log(dt)
 }
