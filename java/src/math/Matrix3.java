700420e084553e3254dd08683b97709829c703dc
Kenaj08 <jan.borys.08@interia.pl>
2025-02-10 20:33:01 +0100

    Changed: + Matrix2 ~ Added toString() to Matrix3

--- java/src/math/Matrix3.java
+++ java/src/math/Matrix3.java
@@ -72,6 +72,13 @@ public class Matrix3{
     double[] toArray(){
         return new double[]{this.m00, this.m10, this.m20, this.m01, this.m11, this.m21, this.m02, this.m12, this.m22};
     }
+    @Override
+    public String toString() {
+        return 
+        this.m00 + this.m01 + this.m02 + "\n" + 
+        this.m10 + this.m11 + this.m12 + "\n" + 
+        this.m20 + this.m21 + this.m22;
+    }
     // toFloatArray(){
     //     return new Float32Array(this.toArray())
     // }
